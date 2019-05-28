47634f79b9a3c169a570c634ef9546eec92a4cb2
Shamri <shamri94@gmail.com>
2019-05-28 17:38:32 +0200

    Delete startJob2.sh

--- JOB2/startJob2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mapred streaming \
-	-D stream.num.map.output.key.fields=2 \
-	-files mapperJob2.py,reducerJob2.py,historical_stocks.csv \
-	-mapper mapperJob2.py \
-	-reducer reducerJob2.py \
-	-input input/first_project_input/historical_stock_prices.csv \
-	-output output/first_project_output/JOB2_Total
