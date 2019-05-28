5a983740c2d6463256a1c57508f58843210270cc
Shamri <shamri94@gmail.com>
2019-05-28 17:35:07 +0200

    Delete startJob1.sh

--- JOB1/startJob1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mapred streaming \
-	-D stream.num.map.output.key.fields=2 \
-	-files mapperJob1.py,reducerJob1.py \
-	-mapper mapperJob1.py \
-	-reducer reducerJob1.py \
-	-input input/first_project_input/historical_stock_prices.csv \
-	-output /user/shamri/output/first_project_output/JOB1_Total
